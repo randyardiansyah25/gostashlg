d6eda0bb6ea02295c36fa233a0daae2af5b0a234
randyardiansyah25 <detwentyfive@gmail.com>
2024-08-05 21:40:31 +0700

    add test error

--- sample/main.go
+++ sample/main.go
@@ -1,13 +1,76 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"runtime"
 
+	"github.com/kpango/glg"
 	"github.com/randyardiansyah25/gostashlg"
 )
 
 func main() {
 	useDefine()
+	var input string
+	// glg.Error("cobain deh")
+	// glg.CustomLog("INFO", "hello--world")
+	// glg.Error(TestError())
+
+	err := ErrLevel3()
+	//el := handleErrorLog(err)
+	glg.Error(err)
+	fmt.Scanln(&input)
+}
+
+func TestError() error {
+	file, line := GetCallerInfo()
+	s := fmt.Sprintf("Called from file: %s, line: %d\n", file, line)
+	return errors.New(s)
+}
+
+func ErrLevel3() error {
+	err := ErrLevel2()
+	return AddError(err, "error level3")
+	//return errors.Join(err, errors.New("err level3"))
+	//return errors.Join(err,errors.New("this third error"))
+}
+
+func ErrLevel2() error {
+	err := ErrLevel1()
+	return AddError(err, "error level2")
+	// return errors.Join(err, errors.New("err level2"))
+
+	// return errors.Join(err, errors.New("this second error"))
+}
+
+func ErrLevel1() error {
+	return errors.New("This first error")
+}
+
+func handleErrorLog(err error) string {
+	// if (errors.Is(err, &errors.Error{})) {
+	// 	return err.(*errors.Error).ErrorStack()
+	// } else {
+	// 	return errors.New(err).ErrorStack()
+	// }
+	return ""
+}
+
+func AddError(er error, message string) error {
+	file, line := GetCallerInfo()
+	s := fmt.Sprintf("%s\n\t%s:%d", message, file, line)
+	return errors.Join(er, errors.New(s))
+}
+
+func GetCallerInfo() (string, int) {
+	// `2` adalah level call stack (1 adalah fungsi GetCallerInfo, 2 adalah fungsi pemanggil)
+	_, file, line, ok := runtime.Caller(2)
+	if !ok {
+		return "Unknown", 0
+	}
+
+	//file = filepath.Base(file)
+	return file, line
 }
 
 func useDefine() {
@@ -34,9 +97,6 @@ func useDefine() {
 
 	log.Put(field,
 		false)
-
-	var input string
-	fmt.Scanln(&input)
 }
 
 func useDefault() {
